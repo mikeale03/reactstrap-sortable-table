24fd7d3ff2f2f303fe20a45a6e7341afaddc62d4
mikeale03 <alemike03@gmail.com>
2022-02-16 20:29:51 +0800

    update readme and add docs

--- src/stories/Table.stories.js
+++ src/stories/Table.stories.js
@@ -31,43 +31,52 @@ export const SampleTable = () => {
 
     const [data, setData] = useState([
         {
-            header1: 'mike', header2: '531', header3: '10/12/2001', header4: 'kigz'
+            header1: 'Jack21', header2: 4486, header3: '10/12/2001', header4: 'Kigz'
         },
         {
-            header1: 'ale', header2: 'mike', header3: '12/12/2000', header4: 'W3Schools'
+            header1: 'Jack12', header2: 5896, header3: '12/12/2000', header4: 'W3Schools'
         },
         {
-            header1: 'ale', header2: '214', header3: '9/12/2000', header4: 'jake'
+            header1: 'Cole', header2: 348, header3: '9/12/2000', header4: 'Jake'
         },
         {
-            header1: '1leddf', header2: '12', header3: '1/12/2011', header4: 'kwaks'
+            header1: 'Hugh', header2: 452, header3: '1/12/2011', header4: 'Mike'
         },
         {
-            header1: '1leddf', header2: '12', header3: '1/12/2011', header4: 'kwaks'
+            header1: 'Ryan', header2: 467, header3: '1/12/2011', header4: 'Ale'
         },
     ])
 
     return (
         <SortableTable 
             bordered
-            variant='dark'
+            striped
             data={data}
             setData={setData}
             columns={columns}
             setColumns={setColumns}
-            addProps={ { tData: {className:'text-danger'} } }
             dateColumns={['header3']}
-            noSortColumns={['header2', 'header4']}
+            noSortColumns={['header4']}
+
             firstColumnRender={(data, index) => {
-                return index % 2 && <Button>{data.header2}</Button>
+                return index % 2 ? <Button>{index + 1}</Button> : <Button variant="secondary">{index + 1}</Button>
             }}
+
             firstColumnLabel="first column"
-            firstColumnHeaderProp={ { style:{width: '50px'} } }
+            firstColumnHeaderProp={ { style:{width: '100px'} } }
+
             lastColumnRender={(data, index) => {
-                return index % 2 === 0 ? <Button>{data.header3}</Button> : undefined
+                return index % 2 === 0 ? 
+                <div className="d-flex justify-content align-items-center flex-column">
+                    <Button>{data.header1}</Button>
+                </div>
+                 : 
+                undefined
             }}
+
             lastColumnLabel="last column"
-            lastColumnHeaderProp={ { style:{width: '1000px'} } }
+            lastColumnHeaderProp={ { className: 'bg-primary text-center' } }
+
             columnRender={[
                 {
                     column: 'header4',
@@ -75,6 +84,7 @@ export const SampleTable = () => {
                         value ==="W3Schools" ? <a href="https://www.w3schools.com/" target="_blank">Visit {value}!</a> : value
                 }
             ]}
+
             addProps={
                 {
                     tBodyRow: (data, index) => index % 2 && {className: 'text-primary'},
